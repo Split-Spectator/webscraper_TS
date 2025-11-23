96bb1444a98ff6d5fd13f63579c1e41c355f4d87
Split-Spectator <aosc9vma@mailer.me>
2025-11-23 12:03:55 -0500

    Max Pages

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,38 @@
-import { argv } from 'node:process';
 import { crawlSiteAsync } from "./crawl";
 
-
 async function main() {
-    let arg = process.argv;
-    let base = arg[2];
-    if (arg.length !== 3 ) {
-        console.log("Usage: npm run start <BASE_URL>");
-        process.exit(1);
-    }
-   
-    console.log(`Beginning crawl of ${base}`)
-    const pages = await crawlSiteAsync(base);
+  if (process.argv.length < 5) {
+    console.log("not enough arguments provided");
+    console.log(
+      "usage: node dist/index.js <baseURL> <maxConcurrency> <maxPages>",
+    );
+    process.exit(1);
+  }
+  if (process.argv.length > 5) {
+    console.log("too many arguments provided");
+    process.exit(1);
+  }
 
-    console.log(pages);;
-   }
+  const baseURL = process.argv[2];
+  const maxConcurrency = Number(process.argv[3]);
+  const maxPages = Number(process.argv[4]);
 
-  main();
-  
+  if (!Number.isFinite(maxConcurrency) || maxConcurrency <= 0) {
+    console.log("invalid maxConcurrency");
+    process.exit(1);
+  }
+  if (!Number.isFinite(maxPages) || maxPages <= 0) {
+    console.log("invalid maxPages");
+    process.exit(1);
+  }
+
+  console.log(
+    `starting crawl of: ${baseURL} (concurrency=${maxConcurrency}, maxPages=${maxPages})...`,
+  );
+
+  await crawlSiteAsync(baseURL, maxConcurrency, maxPages);
+
+  process.exit(0);
+}
+
+main();
