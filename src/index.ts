6dbbd66f0fd29abc8ea50832fa29556f153c618d
Split-Spectator <aosc9vma@mailer.me>
2025-11-22 19:49:53 -0500

    Structure Page Data & main

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+import { argv } from 'node:process';
+
+
+function main() {
+    let arg = process.argv;
+    let base = arg[2];
+    if (arg.length !== 3 ) {
+        console.log("Usage: npm run start <BASE_URL>");
+        process.exit(1);
+    }
+   
+    console.log(`Beginning crawl of ${base}`)
+    process.exit(0);
+   }
+
+  main();
+  
